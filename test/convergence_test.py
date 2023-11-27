738588f6f85353137866e27eedff6a1e64a320bd
Daniel Shapero <shapero.daniel@gmail.com>
2023-11-27 09:32:06 -0800

    Use multiple degrees in convergence test

--- test/convergence_test.py
+++ test/convergence_test.py
@@ -43,94 +43,93 @@ def friction(x):
     return α * (ρ_I * g * (h0 - dh * x / Lx)) * exact_u(x) ** (-1 / m)
 
 
-print("Starting convergence test!")
-degree = 1
-errors, mesh_sizes = [], []
-k_min, k_max, num_steps = 4, 8, 9
-for nx in np.logspace(k_min, k_max, num_steps, base=2, dtype=int):
-    mesh = firedrake.RectangleMesh(nx, nx, float(Lx), float(Ly), diagonal="crossed")
-    x, y = firedrake.SpatialCoordinate(mesh)
+for degree in [1, 2]:
+    errors, mesh_sizes = [], []
+    k_min, k_max, num_steps = 5 - degree, 8 - degree, 9
+    for nx in np.logspace(k_min, k_max, num_steps, base=2, dtype=int):
+        mesh = firedrake.RectangleMesh(nx, nx, float(Lx), float(Ly), diagonal="crossed")
+        x, y = firedrake.SpatialCoordinate(mesh)
 
-    cg = firedrake.FiniteElement("CG", "triangle", degree)
-    dg = firedrake.FiniteElement("DG", "triangle", degree - 1)
-    Q = firedrake.FunctionSpace(mesh, cg)
-    V = firedrake.VectorFunctionSpace(mesh, cg)
-    Σ = firedrake.TensorFunctionSpace(mesh, dg, symmetry=True)
-    # TODO: investigate using DG for this?
-    T = firedrake.VectorFunctionSpace(mesh, cg)
-    Z = V * Σ * T
-    z = firedrake.Function(Z)
-    z.sub(0).assign(Constant((u_inflow, 0)))
+        cg = firedrake.FiniteElement("CG", "triangle", degree)
+        dg = firedrake.FiniteElement("DG", "triangle", degree - 1)
+        Q = firedrake.FunctionSpace(mesh, cg)
+        V = firedrake.VectorFunctionSpace(mesh, cg)
+        Σ = firedrake.TensorFunctionSpace(mesh, dg, symmetry=True)
+        # TODO: investigate using DG for this?
+        T = firedrake.VectorFunctionSpace(mesh, cg)
+        Z = V * Σ * T
+        z = firedrake.Function(Z)
+        z.sub(0).assign(Constant((u_inflow, 0)))
 
-    u_exact = interpolate(as_vector((exact_u(x), 0)), V)
+        u_exact = interpolate(as_vector((exact_u(x), 0)), V)
 
-    h = interpolate(h0 - dh * x / Lx, Q)
-    ds = (1 + β) * ρ / ρ_I * dh
-    s = interpolate(d + h0 - dh + ds * (1 - x / Lx), Q)
+        h = interpolate(h0 - dh * x / Lx, Q)
+        ds = (1 + β) * ρ / ρ_I * dh
+        s = interpolate(d + h0 - dh + ds * (1 - x / Lx), Q)
 
-    # TODO: adjust the yield stress so that this has a more sensible value
-    C = interpolate(friction(x), Q)
-    u_c = interpolate((τ_c / C)**m, Q)
+        # TODO: adjust the yield stress so that this has a more sensible value
+        C = interpolate(friction(x), Q)
+        u_c = interpolate((τ_c / C)**m, Q)
 
-    inflow_ids = (1,)
-    outflow_ids = (2,)
-    side_wall_ids = (3, 4)
+        inflow_ids = (1,)
+        outflow_ids = (2,)
+        side_wall_ids = (3, 4)
 
-    u, M, τ = firedrake.split(z)
-    kwargs = {
-        "velocity": u,
-        "membrane_stress": M,
-        "basal_stress": τ,
-        "thickness": h,
-        "surface": s,
-        "viscous_yield_strain": ε_c,
-        "viscous_yield_stress": τ_c,
-        "friction_yield_speed": u_c,
-        "friction_yield_stress": τ_c,
-        "outflow_ids": outflow_ids,
-    }
-
-    inflow_bc = firedrake.DirichletBC(Z.sub(0), Constant((u_inflow, 0)), inflow_ids)
-    side_wall_bc = firedrake.DirichletBC(Z.sub(0).sub(1), 0, side_wall_ids)
-    bcs = [inflow_bc, side_wall_bc]
-
-    fns = [
-        model.viscous_power,
-        model.friction_power,
-        model.boundary,
-        model.constraint,
-    ]
-
-    J_l = sum(
-        fn(**kwargs, flow_law_exponent=1, sliding_law_exponent=1) for fn in fns
-    )
-    F_l = firedrake.derivative(J_l, z)
-    firedrake.solve(F_l == 0, z, bcs=bcs)
-
-    qdegree = max(8, degree ** n)
-    params = {
-        "form_compiler_parameters": {"quadrature_degree": qdegree}
-    }
-
-    num_continuation_steps = 4
-    for t in np.linspace(0.0, 1.0, num_continuation_steps):
-        exponents = {
-            "flow_law_exponent": Constant((1 - t) + t * n),
-            "sliding_law_exponent": Constant((1 - t) + t * m),
+        u, M, τ = firedrake.split(z)
+        kwargs = {
+            "velocity": u,
+            "membrane_stress": M,
+            "basal_stress": τ,
+            "thickness": h,
+            "surface": s,
+            "viscous_yield_strain": ε_c,
+            "viscous_yield_stress": τ_c,
+            "friction_yield_speed": u_c,
+            "friction_yield_stress": τ_c,
+            "outflow_ids": outflow_ids,
         }
-        J = sum(fn(**kwargs, **exponents) for fn in fns)
-        F = firedrake.derivative(J, z)
-        firedrake.solve(F == 0, z, bcs=bcs, **params)
 
-    u, M, τ = z.subfunctions
-    error = firedrake.norm(u - u_exact) / firedrake.norm(u_exact)
-    δx = mesh.cell_sizes.dat.data_ro.min()
-    mesh_sizes.append(δx)
-    errors.append(error)
-    print(".", end="", flush=True)
+        inflow_bc = firedrake.DirichletBC(Z.sub(0), Constant((u_inflow, 0)), inflow_ids)
+        side_wall_bc = firedrake.DirichletBC(Z.sub(0).sub(1), 0, side_wall_ids)
+        bcs = [inflow_bc, side_wall_bc]
 
-log_mesh_sizes = np.log2(np.array(mesh_sizes))
-log_errors = np.log2(np.array(errors))
-slope, intercept = np.polyfit(log_mesh_sizes, log_errors, 1)
-print(f"log(error) ~= {slope:g} * log(dx) {intercept:+g}")
-assert slope > degree + 0.9
+        fns = [
+            model.viscous_power,
+            model.friction_power,
+            model.boundary,
+            model.constraint,
+        ]
+
+        J_l = sum(
+            fn(**kwargs, flow_law_exponent=1, sliding_law_exponent=1) for fn in fns
+        )
+        F_l = firedrake.derivative(J_l, z)
+        firedrake.solve(F_l == 0, z, bcs=bcs)
+
+        qdegree = max(8, degree ** n)
+        params = {
+            "form_compiler_parameters": {"quadrature_degree": qdegree}
+        }
+
+        num_continuation_steps = 4
+        for t in np.linspace(0.0, 1.0, num_continuation_steps):
+            exponents = {
+                "flow_law_exponent": Constant((1 - t) + t * n),
+                "sliding_law_exponent": Constant((1 - t) + t * m),
+            }
+            J = sum(fn(**kwargs, **exponents) for fn in fns)
+            F = firedrake.derivative(J, z)
+            firedrake.solve(F == 0, z, bcs=bcs, **params)
+
+        u, M, τ = z.subfunctions
+        error = firedrake.norm(u - u_exact) / firedrake.norm(u_exact)
+        δx = mesh.cell_sizes.dat.data_ro.min()
+        mesh_sizes.append(δx)
+        errors.append(error)
+        print(".", end="", flush=True)
+
+    log_mesh_sizes = np.log2(np.array(mesh_sizes))
+    log_errors = np.log2(np.array(errors))
+    slope, intercept = np.polyfit(log_mesh_sizes, log_errors, 1)
+    print(f"degree {degree}: log(error) ~= {slope:g} * log(dx) {intercept:+g}")
+    assert slope > degree + 0.9
