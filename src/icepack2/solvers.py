d965e2d7725c5ee735d540033367bbfe3e4c5f73
Daniel Shapero <shapero.daniel@gmail.com>
2023-12-12 20:43:23 -0800

    Fix up automated parameter tuning

--- src/icepack2/solvers.py
+++ src/icepack2/solvers.py
@@ -2,18 +2,21 @@ import firedrake
 
 __all__ = ["try_regularized_solve"]
 
-def try_regularized_solve(solver, state, parameter):
+def try_regularized_solve(solver, state, parameter, verbose=False):
     state_initial = state.copy(deepcopy=True)
     converged = False
     while not converged:
         try:
+            if verbose:
+                print(float(parameter))
             solver.solve()
             converged = True
         except firedrake.ConvergenceError as error:
             message = str(error)
             if "DIVERGED_MAX_IT" in message:
+                state_initial.assign(state)
                 parameter.assign(parameter / 2)
             elif "DIVERGED_DTOL" in message:
                 state.assign(state_initial)
-                parameter.assign(2 * parameter)
+                parameter.assign(3 * parameter / 2)
 
