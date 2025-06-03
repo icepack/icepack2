c437add3efabbe9ea81d2dda4ea857a9fd17cee0
Daniel Shapero <shapero.daniel@gmail.com>
2025-06-03 09:49:41 -0700

    Add frontal ablation arg to mass balance
    But it breaks the mass balance test. This is weird because the added
    source term is zero??

--- src/icepack2/model/__init__.py
+++ src/icepack2/model/__init__.py
@@ -10,18 +10,19 @@ def mass_balance(**kwargs):
     r"""Return the mass balance equation"""
     field_names = ("thickness", "velocity", "accumulation", "test_function")
     h, u, a, φ = map(kwargs.get, field_names)
-    h_inflow = kwargs.get("thickness_inflow", Constant(0.0))
+    h_in = kwargs.get("thickness_inflow", Constant(0.0))
+    f_c = kwargs.get("frontal_ablation", Constant((0.0, 0.0)))
 
-    cell_balance = (Dt(h) * φ - inner(h * u, grad(φ)) - a * φ) * dx
+    cell_balance = (Dt(h) * φ - inner(h * u - f_c, grad(φ)) - a * φ) * dx
 
     mesh = ufl.domain.extract_unique_domain(h)
     ν = FacetNormal(mesh)
-    f = h * max_value(0, inner(u, ν))
+    F = max_value(0, inner(h * u - f_c, ν))
 
-    outflow = f * φ * ds
-    inflow = h_inflow * min_value(0, inner(u, ν)) * φ * ds
+    outflow = F * φ * ds
+    inflow = h_in * min_value(0, inner(u, ν)) * φ * ds
     boundary_balance = inflow + outflow
 
-    facet_balance = jump(f) * jump(φ) * dS
+    facet_balance = jump(F) * jump(φ) * dS
 
     return cell_balance + facet_balance + boundary_balance
