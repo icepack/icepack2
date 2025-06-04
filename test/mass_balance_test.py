96f635e1291d3463914afed5afd4d6d4789fccff
Daniel Shapero <shapero.daniel@gmail.com>
2025-06-03 17:29:44 -0700

    Revert changes to mass balance, use discontinuous velocity
    The changes to the mass balance function were making the tests fail
    because we don't actually want the factor of `h` inside the `max_value`
    that defines the upwind flux. We can get some of the effect using a
    discontinuous velocity field but all this does is change the thickness
    downstream to its upstream value * (u / (u + u_c)) which is not zero.

--- test/mass_balance_test.py
+++ test/mass_balance_test.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy import pi as π
 import pytest
 import firedrake
-from firedrake import dx, inner, max_value, Constant
+from firedrake import dx, inner, max_value, Constant, conditional
 import irksome
 from icepack2 import model
 
@@ -49,19 +49,12 @@ def test_convergence_rate(degree):
         dt = firedrake.Constant(δt)
         final_time = 2 * π
 
-        lower, upper = firedrake.Function(Q), firedrake.Function(Q)
-        upper.assign(+np.inf)
-        bounds = ("stage", lower, upper)
         params = {
             "solver_parameters": {
-                "snes_type": "vinewtonrsls",
+                "snes_type": "ksponly",
                 "ksp_type": "gmres",
-                "pc_type": "lu",
-                "pc_factor_mat_solver_type": "mumps",
+                "pc_type": "bjacobi",
             },
-            "stage_type": "value",
-            "basis_type": "Bernstein",
-            "bounds": bounds,
         }
         solver = irksome.TimeStepper(problem, tableau, t, dt, h, **params)
 
@@ -80,3 +73,81 @@ def test_convergence_rate(degree):
     slope, intercept = np.polyfit(log_mesh_sizes, log_errors, 1)
     print(f"degree {degree}: log(error) ~= {slope:g} * log(dx) {intercept:+g}")
     assert slope > degree - 0.55
+
+
+@pytest.mark.parametrize("degree", [0, 1])
+def test_frontal_ablation(degree):
+    errors, mesh_sizes = [], []
+    k_min, k_max, num_steps = 5 - degree, 8 - degree, 9
+    for nx in np.logspace(k_min, k_max, num_steps, base=2, dtype=int):
+        mesh = firedrake.UnitSquareMesh(nx, nx, quadrilateral=True)
+        x = firedrake.SpatialCoordinate(mesh)
+
+        element = firedrake.FiniteElement("DQ", "quadrilateral", degree)
+        # Try Bernstein?
+
+        Q = firedrake.FunctionSpace(mesh, element)
+
+        u_max = Constant(1.0)
+        u = Constant((u_max, 0.0))
+
+        h_in = Constant(1.0)
+        x = firedrake.SpatialCoordinate(mesh)
+        L = Constant(0.25)
+        expr = conditional(x[0] < L, h_in, 0.0)
+        h_0 = firedrake.Function(Q).project(expr)
+        h = h_0.copy(deepcopy=True)
+
+        a = Constant(0.0)
+
+        U_c = Constant(0.5)
+        x_c = Constant(0.5)
+        u_c = firedrake.as_vector((conditional(x[0] >= x_c, U_c, 0), 0))
+
+        h_exact = conditional(x[0] <= x_c, h_in, h_in * u_max / (u_max + U_c))
+
+        ϕ = firedrake.TestFunction(Q)
+        problem = model.mass_balance(
+            thickness=h,
+            velocity=u + u_c,
+            accumulation=a,
+            thickness_inflow=h_in,
+            test_function=ϕ,
+        )
+
+        δx = mesh.cell_sizes.dat.data_ro.max()
+        δt = 0.5 * δx / u_max
+
+        t = Constant(0.0)
+        dt = Constant(δt)
+        final_time = 2.0
+
+        lower, upper = firedrake.Function(Q), firedrake.Function(Q)
+        upper.assign(np.inf)
+        params = {
+            "solver_parameters": {
+                "snes_type": "vinewtonrsls",
+                "snes_atol": 1e-12,
+            },
+            "stage_type": "value",
+            "basis_type": "Bernstein",
+            "bounds": ("stage", lower, upper),
+        }
+
+        tableau = irksome.BackwardEuler()
+        solver = irksome.TimeStepper(problem, tableau, t, dt, h, **params)
+
+        while float(t) < final_time:
+            if float(t) + float(dt) > final_time:
+                dt.assign(final_time - float(t))
+            solver.advance()
+            t.assign(t + dt)
+
+        mesh_sizes.append(δx)
+        errors.append(firedrake.assemble(abs(h - h_exact) * dx))
+        print(".", end="", flush=True)
+
+    log_mesh_sizes = np.log2(np.array(mesh_sizes))
+    log_errors = np.log2(np.array(errors))
+    slope, intercept = np.polyfit(log_mesh_sizes, log_errors, 1)
+    print(f"degree {degree}: log(error) ~= {slope:g} * log(dx) {intercept:+g}")
