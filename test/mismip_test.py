a35396ac0023f618817f7a80ed498bc4c3f3daaa
Daniel Shapero <shapero.daniel@gmail.com>
2025-06-18 14:20:59 -0700

    Add more to mismip test

--- test/mismip_test.py
+++ test/mismip_test.py
@@ -39,6 +39,45 @@ def mismip_bed_topography(x):
     return max_value(B_x + B_y, z_deep)
 
 
+def form_momentum_balance(z, w, h, b, H, α, rheo1, rheo3):
+    u, M, τ = z
+    v, N, σ = w
+
+    F_stress_balance = momentum_balance(
+        velocity=u,
+        membrane_stress=M,
+        basal_stress=τ,
+        thickness=h,
+        surface=b + h,
+        test_function=v,
+    )
+
+    F_glen_law = flow_law(
+        velocity=u, membrane_stress=M, thickness=H, **rheo3, test_function=N
+    )
+
+    F_linear_law = α * flow_law(
+        velocity=u, membrane_stress=M, thickness=H, **rheo1, test_function=N
+    )
+
+    F_weertman_drag = friction_law(
+        velocity=u, basal_stress=τ, **rheo3, test_function=σ
+    )
+
+    F_viscous_drag = α * friction_law(
+        velocity=u, basal_stress=τ, **rheo1, test_function=σ
+    )
+
+    return (
+        F_stress_balance
+        + F_glen_law
+        + F_linear_law
+        + F_weertman_drag
+        + F_viscous_drag
+    )
+
+
+
 def run_simulation(ny: int):
     nx = int(lx / ly) * ny
     mesh = firedrake.RectangleMesh(nx, ny, lx, ly, diagonal="crossed")
@@ -66,5 +105,73 @@ def run_simulation(ny: int):
     # Fluidity of ice in yr⁻¹ MPa⁻³
     A = Constant(20.0)
 
+    n = Constant(glen_flow_law)
+    m = Constant(weertman_sliding_law)
+
+    τ_c = Constant(0.1)
+    ε_c = Constant(A * τ_c ** n)
+
+    # Friction coefficient in MPa (m yr⁻¹)⁻¹ᐟ³
+    C = Constant(1e-2)
+    K = C ** (-m) ## TODO: just use 1e6
+    u_c = K * τ_c ** n
+
+    rheo3 = {
+        "flow_law_exponent": n,
+        "flow_law_coefficient": ε_c / τ_c ** n,
+        "sliding_exponent": n,
+        "sliding_coefficient": u_c / τ_c ** n,
+    }
+
+    α = firedrake.Constant(1e-4)
+    rheo1 = {
+        "flow_law_exponent": 1,
+        "flow_law_coefficient": ε_c / τ_c,
+        "sliding_exponent": 1,
+        "sliding_coefficient": u_c / τ_c,
+    }
+
     z = firedrake.Function(Z)
     u, M, τ, h = firedrake.split(z)
+    s = max_value(b + h, (1 - ρ_I / ρ_W) * h)
+    # TODO: figure this one out
+    f = None
+
+    fields = {
+        "velocity": u,
+        "membrane_stress": M,
+        "basal_stress": τ,
+        "thickness": h,
+        "surface": max_value(b + h, (1 - ρ_I / ρ_W) * h),
+        "floating": f,
+    }
+
+    degree = 1
+    qdegree = max(8, degree ** glen_flow_law)
+    pparams = {"form_compiler_parameters": {"quadrature_degree": qdegree}}
+
+    sparams = {
+        "solver_parameters": {
+            "snes_monitor": None,
+            "snes_type": "newtonls",
+            "snes_max_it": 200,
+            "snes_linesearch_type": "nleqerr",
+            "ksp_type": "gmres",
+            "pc_type": "lu",
+            "pc_factor_mat_solver_type": "mumps",
+        },
+    }
+
+    print("Initial momentum solve")
+    v, N, σ, η = firedrake.TestFunctions(Z)
+    F_momentum = form_momentum_balance((u, M, τ), (v, N, σ), h, b, H, α, rheo1, rheo3)
+    F_mass = (h - h_0) * η * dx
+    F = F_momentum + F_mass
+    problem = firedrake.NonlinearVariationalProblem(F, z, **pparams)
+    solver = firedrake.NonlinearVariationalSolver(problem, **sparams)
+
+    num_continuation_steps = 5
+    for r in np.linspace(0.0, 1.0, num_continuation_steps):
+        n.assign((1 - r) + r * glen_flow_law)
+        m.assign((1 - r) + r * weertman_sliding_law)
+        solver.solve()
